a253346582d781f2c01ca4e81652d0e3fad8341d
Siddiqui <aurangzaibsiddiqui@gmail.com>
2017-10-29 19:00:21 +0100

    EKF implementation.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,5 @@
 #include "kalman_filter.h"
+#include "tools.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -18,10 +19,12 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  // prediction state vector
+  x_ = F_ * x_;
+  // state transition matrix
+  MatrixXd Ft = F_.transpose();
+  // prediction uncertainty covariance matrix
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +32,22 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  // sensor measurement
+  VectorXd y = z - (H_ * x_);
+  // measurement function
+  MatrixXd Ht = H_.transpose();
+  // sensor measurement error
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  // kalman filter gain
+  MatrixXd K = P_ * Ht * Si;
+  // new estimate
+  x_ = x_ + (K * y);
+  // identity matrix
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  // new prediction covariance  matrix
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +55,23 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
-}
+  // find the jacobian matrix
+  Tools tools;
+  H_ = tools.CalculateJacobian(x_);
+  // sensor measurement
+  VectorXd y = z - (H_ * x_);
+  // measurement function
+  MatrixXd Ht = H_.transpose();
+  // sensor measurement error
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  // kalman filter gain
+  MatrixXd K = P_ * Ht * Si;
+  // new estimate
+  x_ = x_ + (K * y);
+  // identity matrix
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  // new prediction covariance  matrix
+  P_ = (I - K * H_) * P_;
+};
--- src/tools.cpp
+++ src/tools.cpp
@@ -15,11 +15,47 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if (estimations.empty() || estimations.size() != ground_truth.size()) return rmse;
+  //accumulate squared residuals
+  for (int i = 0; i < estimations.size(); ++i) {
+    for (int j = 0; j < rmse.size(); j++)
+      rmse[j] += pow(estimations[i][j] - ground_truth[i][j], 2);
+  }
+  // root mean residuals
+  for (int i = 0; i < rmse.size(); i++) {
+    //calculate the mean
+    rmse[i] /= estimations.size();
+    //calculate the squared root
+    rmse[i] = sqrt(rmse[i]);
+  }
+  //return the result
+  return rmse;
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
   /**
   TODO:
     * Calculate a Jacobian here.
   */
+  MatrixXd Hj(3, 4);
+  //recover state parameters
+  double px = x_state(0), py = x_state(1), vx = x_state(2), vy = x_state(3);
+
+  //check division by zero
+  double den = pow(px, 2) + pow(py, 2);
+  if (den == 0) return Hj;
+  double den_sqrt = sqrt(den);
+
+  //compute the Jacobian matrix
+  Hj << px / den_sqrt, py / den_sqrt, 0, 0,
+        -py / den, px / den, 0, 0,
+        py * (vx * py - vy * px) / pow(den_sqrt, 3), px * (vy * px - vx * py) / pow(den_sqrt, 3), px / den_sqrt,
+        py / den_sqrt;
+  return Hj;
 }
